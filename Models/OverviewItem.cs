02b33a6bfb0d87e20520be0f5cd8928f3b06c3bb
Yunus Emre AYGÜN <callahan_555@hotmail.com>
2022-08-25 02:14:30 +0300

    Created model folder and added OverviewItem model.

---
+++ Models/OverviewItem.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiFinance.Models;
+
+public class OverviewItem
+{
+    public string IconSource { get; set; }
+
+    public string Title { get; set; }
+
+    public string Details { get; set; }
+
+    public int Amount { get; set; }
+}
